ed8dd2c73f403b3535f9ee81dc095a0e1f471d15
NagariaHussain <hussainbhaitech@gmail.com>
2020-12-10 18:32:38 +0530

    Add food CREATE functionality

--- routes/main.js
+++ routes/main.js
@@ -1,20 +1,57 @@
+// Utility function to convert req body into an Array
+function convertToArray(reqBody) {
+    return [
+        reqBody.food_name, 
+        parseFloat(reqBody.typical_value),
+        reqBody.typical_value_unit,
+        parseFloat(reqBody.calories),
+        parseFloat(reqBody.carbs),
+        parseFloat(reqBody.fats),
+        parseFloat(reqBody.proteins),
+        parseFloat(reqBody.salt),
+        parseFloat(reqBody.sugar)
+    ];
+}
 // Export function to handle routing
 module.exports = function(app) {
     // Index route
     app.get('/', (req, res) => {
-        res.render("index");
+        return res.render("index");
     });
 
     app.get('/add', (req, res) => {
-        res.render("add_food");
+        return res.render("add_food");
+    });
+
+    app.post('/add', (req, res) => {
+        // SQL INSERT statement to add row to foods table
+        const insert_statement = "INSERT INTO foods VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        
+        // Convert request body to Array 
+        // suitable to pass to mysql driver
+        const food_data = convertToArray(req.body);
+
+        // Query the database with data and statment
+        db.query(insert_statement, food_data, (err) => {
+            // If an error occurs
+            if (err) {
+                // Set status to 500 (Internal Server Error)
+                res.status(500).send("Something went wrong with the database.");
+            }
+
+            // No errors
+            // redirect the user to list page
+            // TODO: Show a success message (using a success page maybe?)
+            return res.redirect("/foods");
+        });
     });
 
     app.get('/search', (req, res) => {
-        res.render("search");
+        return res.render("search");
     });
 
     app.get('/about', (req, res) => {
-        res.render("about");
+        return res.render("about");
     });
 
     app.get('/foods', (req, res) => {
@@ -33,13 +70,13 @@ module.exports = function(app) {
             // Everything went well
             // Render food list page with the data
             // from the database
-            res.render("list_food", {foods: foods});
+            return res.render("list_food", {foods: foods});
         });
     }); 
 
 
     // Catch all route for 404 page
     app.get('*', (req, res) => {
-        res.status(404).send("404 page goes here!");
+        return res.status(404).send("404 page goes here!");
     });
 };
